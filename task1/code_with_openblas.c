27bb6fb90975cc152f934113416f0456228043e6
TypeFloat <liuzekun_123@163.com>
2021-05-01 16:33:54 +0800

    提交了使用openblas的代码

---
+++ task1/code_with_openblas.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "cblas.h"
+
+#define WIN
+
+#ifdef WIN
+#include <time.h>
+#else
+#include <sys/time.h>
+#endif
+
+void matrix_multiple(double* A, double* B, double* C, int n);
+void show_matrix(double* C, int n);
+void test(double* A, double* B, double* C, int n);
+void get_matrix(double* A, double* B, int n);
+
+void matrix_multiple(double* A, double* B, double* C, int n){
+    cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, n, n, n, 1, A, n, B, n, 0, C, n);
+}
+
+void show_matrix(double* C, int n){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            printf("%f ", C[i * n + j]);
+        }
+        printf("\n");
+    }
+}
+
+void get_matrix(double* A, double* B, int n){
+    for(int i = 0; i < n * n; i++){
+        A[i] = rand();
+        B[i] = rand();
+    }
+}
+
+#ifdef WIN
+void test(double* A, double* B, double* C, int n){
+    clock_t start = clock();
+    matrix_multiple(A, B, C, n);
+    clock_t end = clock();
+    clock_t time = (end - start) / CLOCKS_PER_SEC;
+    printf("The size of matric is %d x %d\n", n, n);
+    printf("The time is %d\n", time);
+    //printf("The result is \n");
+    //show_matrix(C, n);
+}
+#endif
+
+#ifdef LINUX
+void test(double* A, double* B, double* C, int n){
+    timeval start, end;
+    gettimeofday(&start, 0);
+    matrix_multiple(A, B, C, n);
+    gettimeofday(&end, 0);
+    double time = 1000000 * (end.tv_sec - start.tv_sec) + end.tv_usec - start.tv_usec;
+    time /= 1000;
+    printf("The size of matric is %d x %d\n", n, n);
+    printf("The time is %f\n", time);
+    //printf("The result is \n");
+    //show_matrix(C, n);
+}
+#endif
+
+int main(){
+    int n = 100;
+    while(n <= 2000){
+        double* A = (double*)malloc(sizeof(double) * n * n);
+        double* B = (double*)malloc(sizeof(double) * n * n);
+        double* C = (double*)malloc(sizeof(double) * n * n);
+        get_matrix(A, B, n);
+        test(A, B, C, n);
+        n += 100;
+    }
+    return 0;
+}
