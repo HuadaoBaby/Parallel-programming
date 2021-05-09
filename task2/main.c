91b77e50a929e2cd437cd94c66aa0ccb55f038f5
TypeFloat <liuzekun_123@163.com>
2021-05-09 11:03:21 +0800

    完成了作业的完整代码

--- task2/main.c
+++ task2/main.c
@@ -2,21 +2,18 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <sys/time.h>
+
 #include "omp.h"
 
-#define WIN
-
-#ifdef WIN
-#include <time.h>
-#else
-#include <sys/time.h>
-#endif
-
+void my_sort(int* array, int n);
+void my_sort_omp(int* array, int n);
+void quicksort_omp(int *array, int start, int end);
 void quicksort(int *array, int start, int end);
 int partition(int *array, int start, int end);
 void swap(int *a, int *b);
-void quicksort(int* A, int start, int end);
-void quicksort_omp(int* A, int start, int end);
 void mergesort(int* A, int* temp, int start, int end);
 void mergesort_omp(int* A, int* temp, int start, int end);
 void show_vector(int* A, int n);
@@ -30,21 +27,22 @@ void show_vector(int* A, int n){
 }
 
 void get_vector(int* A, int n, int mode){
+    srand(time(NULL));
     if(mode == 0){
         A[0] = rand();
         for(int i = 1; i < n; i++){
-            A[i] = rand()%5 + A[i - 1];
+            A[i] = rand()%n + A[i - 1];
         }
     }
     else if(mode == 1){
         for(int i = 0; i < n; i++){
-            A[i] = rand()%5 + A[i - 1];
+            A[i] = rand()%n + A[i - 1];
         }
     }
     else{
         A[0] = rand();
         for(int i = 1; i < n; i++){
-            A[i] = rand()%5 - A[i - 1];
+            A[i] = rand()%n - A[i - 1];
         }
     }
     printf("Create vector, successfully, mode is %d\n", mode);
@@ -79,15 +77,18 @@ void quicksort(int *array, int start, int end){
 void quicksort_omp(int *array, int start, int end){
     if (start >= end) return;
     int index = partition(array, start, end);
-    if (end - start < 1500){
-        quicksort_omp(array, start, index - 1);
-        quicksort_omp(array, index + 1, end);
-    }
-    else{
-        #pragma omp task
-        quicksort_omp(array, start, index - 1);
-        #pragma omp task
-        quicksort_omp(array, index + 1, end);
+    #pragma omp parallel sections shared(array)
+    {
+        #pragma omp section
+        {
+            quicksort(array, start, index - 1);
+            //printf("running 1\n");
+        }
+        #pragma omp section
+        {
+            //printf("running 2\n");
+            quicksort(array, index + 1, end);
+        }
     }
 }
 
@@ -132,16 +133,16 @@ void mergesort_omp(int* A, int* temp, int start, int end){
     int end1 = mid;
     int start2 = mid + 1;
     int end2 = end;
-    if(end - start < 1500){
-        mergesort(A, temp, start1, end1);
-        mergesort(A, temp, start2, end2);
-    }
-    else{
-        #pragma omp task
-        mergesort_omp(A, temp, start1, end1);
-        #pragma omp task
-        mergesort_omp(A, temp, start2, end2);
-        #pragma omp taskwait
+    #pragma omp parallel sections
+    {
+        #pragma omp section
+        {
+            mergesort(A, temp, start1, end1);
+        }
+        #pragma omp section
+        {
+            mergesort(A, temp, start2, end2);
+        }
     }
     int pointer = start;
     while(start1 <= end1 && start2 <= end2){
@@ -168,92 +169,104 @@ void mergesort_omp(int* A, int* temp, int start, int end){
     }
 }
 
-#ifdef WIN
-void test(int* A, int n){
+void my_sort(int* array, int n){
     int* temp = (int*)malloc(sizeof(int) * n);
-    printf("The size of vector is %d\n", n);
-    clock_t start1 = clock();
-    quicksort(A, 0, n-1);
-    clock_t end1 = clock();
-    clock_t time1 = (end1 - start1) / CLOCKS_PER_SEC;
-    printf("The quicksort time is %d\n", time1);
-    
-    clock_t start2 = clock();
-    quicksort_omp(A, 0, n-1);
-    clock_t end2 = clock();
-    clock_t time2 = (end2 - start2) / CLOCKS_PER_SEC;
-    printf("The quicksort_omp time is %d\n", time2);
-    
-    clock_t start3 = clock();
-    mergesort(A, temp, 0, n-1);
-    clock_t end3 = clock();
-    clock_t time3 = (end3 - start3) / CLOCKS_PER_SEC;
-    printf("The mergesort time is %d\n", time3);
-
-    clock_t start4 = clock();
-    mergesort_omp(A, temp, 0, n-1);
-    clock_t end4 = clock();
-    clock_t time4 = (end4 - start4) / CLOCKS_PER_SEC;
-    printf("The mergesort_omp time is %d\n", time4);
-    //printf("The result is \n");
-    //show_matrix(C, n);
+    memcpy(temp, array, sizeof(int) * n);
+    for (int i = 0; i < n; i++){
+        int count = 0;
+        for (int j = 0; j < n; j++){
+            if (temp[i] > temp[j])
+                count++;
+        }
+        array[count] = temp[i];
+    }
 }
-#endif
 
-#ifdef LINUX
-void test(int*A int n){
+
+void my_sort_omp(int* array, int n){
     int* temp = (int*)malloc(sizeof(int) * n);
+    memcpy(temp, array, sizeof(int) * n);
+    #pragma omp parallel for
+    for (int i = 0; i < n; i++){
+        int count = 0;
+        for (int j = 0; j < n; j++){
+            if (temp[i] > temp[j])
+                count++;
+        }
+        array[count] = temp[i];
+    }
+}
+
+void test(int*A, int n){
+    int* temp = (int*)malloc(sizeof(int) * n);
+    int* array = (int*)malloc(sizeof(int) * n);
     printf("The size of vector is %d\n", n);
     struct timeval start, end;
 
+    memcpy(array ,A, sizeof(int) * n);
     gettimeofday(&start, 0);
-    quicksort(A, 0, n-1);
+    quicksort(array, 0, n-1);
     gettimeofday(&end, 0);
-    double time = 1000000 * (end.tv_sec - start.tv_sec) + end.tv_usec - start.tv_usec;
-    time /= 1000;
-    printf("The quicksort time is %f\n", time);
+    double time1 = 1000000 * (end.tv_sec - start.tv_sec) + end.tv_usec - start.tv_usec;
+    time1 /= 1000;
+    printf("The quicksort time is %f\n", time1);
 
+    memcpy(array ,A, sizeof(int) * n);
     gettimeofday(&start, 0);
-    quicksort_omp(A, 0, n-1);
+    quicksort_omp(array, 0, n-1);
     gettimeofday(&end, 0);
-    double time = 1000000 * (end.tv_sec - start.tv_sec) + end.tv_usec - start.tv_usec;
-    time /= 1000;
-    printf("The quicksort_omp time is %f\n", time);
+    double time2 = 1000000 * (end.tv_sec - start.tv_sec) + end.tv_usec - start.tv_usec;
+    time2 /= 1000;
+    printf("The quicksort_omp time is %f\n", time2);
 
+    memcpy(array ,A, sizeof(int) * n);
     gettimeofday(&start, 0);
-    mergesort(A, temp, 0, n-1);
+    mergesort(array, temp, 0, n-1);
     gettimeofday(&end, 0);
-    double time = 1000000 * (end.tv_sec - start.tv_sec) + end.tv_usec - start.tv_usec;
-    time /= 1000;
-    printf("The mergesort time is %f\n", time);
+    double time3 = 1000000 * (end.tv_sec - start.tv_sec) + end.tv_usec - start.tv_usec;
+    time3 /= 1000;
+    printf("The mergesort time is %f\n", time3);
 
+    memcpy(array ,A, sizeof(int) * n);
     gettimeofday(&start, 0);
-    mergesort_omp(A, temp, 0, n-1);
+    mergesort_omp(array, temp, 0, n-1);
     gettimeofday(&end, 0);
-    double time = 1000000 * (end.tv_sec - start.tv_sec) + end.tv_usec - start.tv_usec;
-    time /= 1000;
-    printf("The mergesort_omp time is %f\n", time);
+    double time4 = 1000000 * (end.tv_sec - start.tv_sec) + end.tv_usec - start.tv_usec;
+    time4 /= 1000;
+    printf("The mergesort_omp time is %f\n", time4);
 
-    //printf("The result is \n");
-    //show_matrix(C, n);
+    memcpy(array ,A, sizeof(int) * n);
+    gettimeofday(&start, 0);
+    my_sort(array, n);
+    gettimeofday(&end, 0);
+    double time5 = 1000000 * (end.tv_sec - start.tv_sec) + end.tv_usec - start.tv_usec;
+    time4 /= 1000;
+    printf("The my_sort time is %f\n", time5);
+
+    memcpy(array ,A, sizeof(int) * n);
+    gettimeofday(&start, 0);
+    my_sort_omp(array, n);
+    gettimeofday(&end, 0);
+    double time6 = 1000000 * (end.tv_sec - start.tv_sec) + end.tv_usec - start.tv_usec;
+    time4 /= 1000;
+    printf("The my_sort_omp time is %f\n", time6);
+
+    free(temp);
+
+    FILE *file = fopen("./time.txt", "a");
+    fprintf(file, "%f,%f,%f,%f,%f\n", time1, time2, time3, time4,time5);
+    fclose(file);
 }
-#endif
 
 int main(){
     for(int n = 10; n <= 100000000; n *= 10){
         int* A = (int*)malloc(sizeof(int) * n);
-        for(int i = 0; i < 8; i++){
-            get_vector(A, n, 0);
-            test(A, n);
-        }
-        for(int i = 0; i < 8; i++){
-            get_vector(A, n, 1);
-            test(A, n);
-        }
-        for(int i = 0; i < 8; i++){
-            get_vector(A, n, 2);
-            test(A, n);
-        }
+        get_vector(A, n, 0);
+        test(A, n);
+        get_vector(A, n, 1);
+        test(A, n);
+        get_vector(A, n, 2);
+        test(A, n);
     }
     return 0;
 }
